6c0de80fb522a3c0efad205da918cf69b55729c2
Bahadır Enes Hamevioğlu <bhdrhmvgl@gmail.com>
2023-06-13 17:31:36 +0300

    footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,7 +1,14 @@
 "use client";
+import Link from "next/link";
+
 import styles from "./footer.module.scss";
 function Footer() {
-  return <footer className={styles.footer}>inspired from asd</footer>;
+  return (
+    <footer className={styles.footer}>
+      inspired from 
+      <Link href="https://react-clicker-game.netlify.app/">this game!</Link>
+    </footer>
+  );
 }
 
 export default Footer;
